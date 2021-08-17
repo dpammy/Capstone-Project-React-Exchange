d1a58f255df6b442130b1a2a6ca372a881c89fbe
dpammy <pamela.dianon@amalitech.org>
2021-08-17 13:54:05 +0100

    first commit main

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+// Components
+import Converter from './Components/Converter';
+
+export function App() {
+  return (
+    <div className="App">
+      <Converter />
+    </div>
+  );
+}
+
+export default App;
